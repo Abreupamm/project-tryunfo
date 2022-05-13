30fb3325106463ac6f8787cbbb9995b440fda8e9
pamela <abreupamm@gmail.com>
2022-05-12 22:59:07 -0300

    questão 3 attr 1, 2 e 3

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,12 +3,22 @@ import PropTypes from 'prop-types';
 
 class Card extends React.Component {
   render() {
-  const { cardName, cardImage, cardDescription } = this.props;
+    const {
+      cardName,
+      cardImage,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3
+    } = this.props;
     return (
       <div>
         <p data-testid="name-card">{cardName}</p>
         <img data-testid="image-card" src={cardImage} alt={cardName} />
-        <p data-testid="description-card">{ cardDescription }</p>
+        <p data-testid="description-card">{cardDescription}</p>
+        <p data-testid="attr1-card">{cardAttr1}</p>
+        <p data-testid="attr2-card">{cardAttr2}</p>
+         <p data-testid="attr3-card">{ cardAttr3 }</p>
       </div>
     );
   }
