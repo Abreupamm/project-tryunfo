64f6a7c3876666c22e81c540564a14a7ff33786d
pamela <abreupamm@gmail.com>
2022-05-12 22:54:06 -0300

    questão 3 name-card e image-card

---
+++ src/components/Card.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Card extends React.Component {
+  render() {
+  const { cardName, cardImage } = this.props;
+    return (
+      <div>
+        <p data-testid="name-card">{cardName}</p>
+        <img data-testid="image-card" src={ cardImage } alt={ cardName } />
+      </div>
+    );
+  }
+}
+
+Card.propTypes = {
+cardName: PropTypes.string.isRequired,
+cardDescription: PropTypes.string.isRequired,
+cardAttr1: PropTypes.string.isRequired,
+cardAttr2: PropTypes.string.isRequired,
+cardAttr3: PropTypes.string.isRequired,
+cardImage: PropTypes.string.isRequired,
+cardRare: PropTypes.string.isRequired,
+cardTrunfo: PropTypes.bool.isRequired,
+};
+
+export default Card;
