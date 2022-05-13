7cf025f0f76ca75b3e05f4d54a62e83aa10aad9d
pamela <abreupamm@gmail.com>
2022-05-12 23:32:15 -0300

    questão 3 trunfo-card

--- src/components/Card.js
+++ src/components/Card.js
@@ -13,6 +13,7 @@ class Card extends React.Component {
       cardRare,
       cardTrunfo,
     } = this.props;
+
     return (
       <div>
         <p data-testid="name-card">{cardName}</p>
@@ -21,7 +22,8 @@ class Card extends React.Component {
         <p data-testid="attr1-card">{cardAttr1}</p>
         <p data-testid="attr2-card">{cardAttr2}</p>
         <p data-testid="attr3-card">{cardAttr3}</p>
-        <p data-testid="rare-card">{ cardRare }</p>
+        <p data-testid="rare-card">{cardRare}</p>
+        { cardTrunfo && <p data-testid="trunfo-card">Super Trunfo</p> }
       </div>
     );
   }
